8c8bb607c086c7149dd22379ea200cbe90cf5e6d
himanxhu1233 <dhandolehimanshu@gmail.com>
2025-04-23 16:40:10 +0530

    initial commit

---
+++ src/main/java/himanshu/fooddelevery/FoodDeleveryApplication.java
@@ -0,0 +1,13 @@
+package himanshu.fooddelevery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeleveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodDeleveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/himanshu/fooddelevery/FoodDeleveryApplicationTests.java
@@ -0,0 +1,13 @@
+package himanshu.fooddelevery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeleveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
